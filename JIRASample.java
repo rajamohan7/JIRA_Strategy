7e3a056c94b0d963af9317eabc110b31d364eb67
rajamohan7 <34439299+rajamohan7@users.noreply.github.com>
2018-01-29 20:47:28 +0530

    New file added

---
+++ JIRASample.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class JIRASample {
+
+	public static void main(String[] args) {
+		System.out.println("Rajamohan Raju");
+	}
+}
